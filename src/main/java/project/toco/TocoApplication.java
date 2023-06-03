6f5c2464450dbb55649d92f817a9e273c528f11c
Ilpyo-Yang <ilpyoyang@gmail.com>
2023-06-03 12:41:46 +0900

    Initial commit

---
+++ src/main/java/project/toco/TocoApplication.java
@@ -0,0 +1,13 @@
+package project.toco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TocoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/toco/TocoApplicationTests.java
@@ -0,0 +1,13 @@
+package project.toco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
