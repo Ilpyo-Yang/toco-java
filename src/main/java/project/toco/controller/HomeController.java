d7f11c078ab244c04421a0f7cbba06d7a3eb5ec0
Ilpyo-Yang <ilpyoyang@gmail.com>
2023-06-03 22:05:35 +0900

    HomeController, LogController 컨트롤러 추가

--- src/main/java/project/toco/controller/HomeController.java
+++ src/main/java/project/toco/controller/HomeController.java
@@ -1,19 +1,12 @@
 package project.toco.controller;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
-  @GetMapping("/")
-  public String index() {
-    return "/index";
-  }
-
-  @GetMapping("/login")
-  public String login() {
-    return "/login";
-  }
+    @GetMapping(value = {"/", "/index"})
+    public String index(){
+        return "/index";
+    }
 }
---
+++ src/main/java/project/toco/controller/LogController.java
@@ -0,0 +1,20 @@
+package project.toco.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+public class LogController {
+
+    @GetMapping("/login")
+    public String login(){
+        return "/login";
+    }
+
+    @PostMapping("/logout")
+    public String logout(){
+        return "/";
+    }
+}
