0abf18f633a75e0b5d2e267648a0347966dbf450
Ilpyo-Yang <ilpyoyang@gmail.com>
2023-06-03 23:41:54 +0900

    security 허용 url 추가

--- src/main/java/project/toco/config/SecurityConfig.java
+++ src/main/java/project/toco/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http
               .authorizeHttpRequests(authorize -> authorize
-                  .requestMatchers("/", "/login", "/logout", "/css/**", "/js/**", "/font/**").permitAll()
+                  .requestMatchers("/", "/login", "/logout", "/css/**", "/js/**", "/font/**", "/image/**").permitAll()
                   .requestMatchers("/user/**").hasRole("USER")
                   .requestMatchers("/admin/**").hasRole("ADMIN")
                   .anyRequest().authenticated()
